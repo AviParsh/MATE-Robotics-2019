afc12c1c4e0f6a1d313d7e023e317035fe92f6ef
praneethkanna <praneeth.v.kanna@gmail.com>
2019-02-11 23:14:00 -0500

    Test Commit

--- src/Interface/main.java
+++ src/Interface/main.java
@@ -3,6 +3,7 @@ package Interface;
 public class main {
     public static void main(String[] args) {
         VisualInterface gui = new VisualInterface();
+        System.out.println("yuh time");
     }
 
 }
