3acf4ae281b4a7978e9932a27e5f07aacb699117
pran21 <praneeth.v.kanna@gmail.com>
2019-02-15 14:34:40 -0500

    Update main.java

--- src/Interface/main.java
+++ src/Interface/main.java
@@ -3,7 +3,6 @@ package Interface;
 public class main {
     public static void main(String[] args) {
         VisualInterface gui = new VisualInterface();
-        System.out.println("yuh time");
     }
 
 }
