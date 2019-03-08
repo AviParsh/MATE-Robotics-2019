cd7bee5a3e4496658face2dad64ffe80e217af9c
AviParsh <avi.parsh01@gmail.com>
2019-03-08 18:55:34 -0500

    fixed angle output

--- src/Interface/Line.java
+++ src/Interface/Line.java
@@ -36,32 +36,26 @@ public class Line {
     public void draw(Graphics2D g){
         g.draw(new Line2D.Double(x1,y1,x2,y2));
     }
-    public double getangle(){
+    public double getangle() {
         double a;
-        double midx = (x1+x2)/2;
-        double midy = (y1+y2)/2;
-        double xdiff = x2-midx;
-        double ydiff = -(y2-midy);
-        a = Math.atan(ydiff/xdiff);
-
-        if(a>=Math.PI/2&&a<=Math.PI){
-            a+=Math.PI/2;
-            System.out.println("second quad");
+        double midx = (x1 + x2) / 2;
+        double midy = (y1 + y2) / 2;
+        double xdiff = (x2 - midx);
+        double ydiff = -(y2 - midy);
 
 
-        }else if(a>=Math.PI&&a<=(3*Math.PI)/2){
 
-            a+=Math.PI/2;
-            System.out.println("Third Quad");
+        a = Math.atan2(ydiff , xdiff);
 
-        }else if(a>=(3*Math.PI/2)&&a<=(2*Math.PI)){
-            System.out.println("fourth");
+        if(a<0){
+            a = Math.PI*2+a;
 
-            a+=Math.PI/2;
-        }else if(a>(2*Math.PI)){
-            a = 0;
         }
-        double deg = Math.toDegrees(a);
+
+
+
+
+       double deg = Math.toDegrees(a);
         System.out.println(deg);
 
 
