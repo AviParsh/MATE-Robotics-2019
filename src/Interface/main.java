5915ae033c7c72606b4e606266cfa1f9bc6f4a17
Vikram <viks.deepak@gmail.com>
2019-03-22 23:39:29 -0400

    established serial connection (input from imu to java console)

--- src/Interface/main.java
+++ src/Interface/main.java
@@ -1,22 +1,27 @@
 package Interface;
-
+import Controller.control;
 //import Serial.Serial;
 //import com.fazecast.jSerialComm.SerialPort;
+
+import Controller.JInputJoystick;
+import net.java.games.input.Controller;
 
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-       // VisualInterface gui = new VisualInterface();
+       //VisualInterface gui = new VisualInterface();
         GUIWindow gui = new GUIWindow();
+        control logitech = new control();
 
        System.out.println("cool");
-//
-//       Serial s = new Serial(1);
-//       int t = 1;
-//       while(t==1) {
-//           System.out.println(s.getPitcb());
-//       }
+        System.out.println(logitech.numButtons());
+       while(true){
+
+           System.out.println(logitech.getButtonVal());
+       }
+
     }
+
 
 }
